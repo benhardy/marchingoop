c643256ad49b54b1b0f2ba4a4c5dd29472c94559
Ben Hardy <benhardy+github@gmail.com>
2017-01-10 07:23:44 -0500

    update Hello to use camera to render basic checker plane

--- src/main/scala/net/aethersanctum/marchingoop/sample/Hello.scala
+++ src/main/scala/net/aethersanctum/marchingoop/sample/Hello.scala
@@ -1,17 +1,28 @@
 package net.aethersanctum.marchingoop.sample
 
-import java.awt.Color.{BLACK, WHITE}
+import java.awt.Color.{BLACK, WHITE, BLUE}
 
-import net.aethersanctum.marchingoop.main.Rendering
+import net.aethersanctum.marchingoop.main.Vector
+import net.aethersanctum.marchingoop.main.Vector.{Y, Z}
+import net.aethersanctum.marchingoop.main.{Camera, Rendering}
 
 object Hello {
   def main(args: Array[String]) = {
     val rendering = new Rendering(640, 480)
 
+    val location = Y * 10
+    val looking = Z
+    val camera = new Camera(rendering, location, looking)
     for (yPixel <- 0 until rendering.screenHeight) {
       for (xPixel <- 0 until rendering.screenWidth) {
-        val bit = ((xPixel ^ yPixel) >> 5) & 1
-        val color = if (bit > 0) WHITE else BLACK
+        val ray = camera.rayForPixel(xPixel, yPixel)
+        val hit = location + Vector(ray.x, 0, ray.z) * location.y / ray.y
+        val color = if (hit.z > 0) {
+          val bit = (hit.x.toInt ^ hit.z.toInt) & 1
+          if (bit > 0) WHITE else BLACK
+        } else {
+          BLUE
+        }
         rendering.setPixel(xPixel, yPixel, color)
       }
     }
